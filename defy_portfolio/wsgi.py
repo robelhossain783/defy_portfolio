f839a6445fcc9e4d0d93f67364eac37c5d484838
Robel Hossain <robel.hossain@upaybd.com>
2026-01-14 17:22:35 +0600

    Expose  variable for Vercel WSGI

--- defy_portfolio/wsgi.py
+++ defy_portfolio/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'defy_portfolio.settings')
 
 application = get_wsgi_application()
+
+# For Vercel
+app = application
