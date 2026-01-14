efcd1d14f219ac05e039ee7da5313fbecc44ff4b
Robel Hossain <robel.hossain@upaybd.com>
2026-01-14 16:00:15 +0600

    fix issue and ready for production

--- defy_portfolio/settings.py
+++ defy_portfolio/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0h+zb5(tu%&uv0!#u(vl7r04jxu!2e3ihej01wkmvfg**8^2pz'
+# SECRET_KEY = '0h+zb5(tu%&uv0!#u(vl7r04jxu!2e3ihej01wkmvfg**8^2pz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+DEBUG = False
+ALLOWED_HOSTS = ['.vercel.app']
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.vercel.app',
+]
 
 # Application definition
 
@@ -107,9 +108,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
 
 
 # Internationalization
